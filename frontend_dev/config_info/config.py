731824386e83305a64825425c824adbe2b03e6fc
CoastLinqt <135663761+CoastLinqt@users.noreply.github.com>
2024-09-20 00:33:16 +0300

    Update config.py

--- frontend_dev/config_info/config.py
+++ frontend_dev/config_info/config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = '6944166033:AAHfi1RNfvEq3bIQewoHjvkHE9Qv5Tiq3iQ'
-BACK_URL = ' https://464d-78-177-175-19.ngrok-free.app'
+BOT_TOKEN = ''
+BACK_URL = ''
 # WEBHOOK_PATH = f"/bot/{BOT_TOKEN}" # формируем системные переменные из переменных окружения
 # WEBHOOK_URL = BACK_URL + WEBHOOK_PATH
 
