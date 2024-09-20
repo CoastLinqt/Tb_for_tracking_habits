3c96b7d6d109698ec58d525b1563d11e150d6fdd
georgii_semchev <dvyl@inbox.ru>
2024-09-20 22:06:59 +0300

    version 0.0.4(UPDATE)

--- frontend_dev/config_info/config.py
+++ frontend_dev/config_info/config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = ''
-BACK_URL = ''
+BOT_TOKEN = '6944166033:AAG-wYQXXufnT1ku77QGdBhVcIv2TvePLvE'
+BACK_URL = 'https://b1e1-78-177-175-19.ngrok-free.app'
 # WEBHOOK_PATH = f"/bot/{BOT_TOKEN}" # формируем системные переменные из переменных окружения
 # WEBHOOK_URL = BACK_URL + WEBHOOK_PATH
 
