4a3542a6665d346c791f79a1f4d56e3007e59dad
georgii_semchev <dvyl@inbox.ru>
2024-09-23 02:03:59 +0300

    Update 1.0.0(Version)

--- frontend_dev/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import edit_habits
-from . import habits
-from . import add_habit
-from . import help
-from . import start
-
--- frontend_dev/utils/set_bot_commands.py
+++
@@ -1,8 +0,0 @@
-from telebot.types import BotCommand
-from frontend_dev.config_info.config import DEFAULT_COMMANDS
-
-
-def set_default_commands(bot):
-    bot.set_my_commands(
-        [BotCommand(*i) for i in DEFAULT_COMMANDS]
-    )
